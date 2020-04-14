7da66ea55c6029f9234d9b343d88bc1b4cd40c88
agor-dot <agorokhov@amt.ru>
2020-04-14 19:20:33 +0300

    Fix missing files.

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -8,19 +8,17 @@ provider "google" {
 }
 
 module "app" {
-  source           = "../modules/app"
-  public_key_path  = var.public_key_path
-  zone             = var.zone
-  app_disk_image   = var.app_disk_image
-  private_key_path = var.private_key_path
+  source          = "../modules/app"
+  public_key_path = var.public_key_path
+  zone            = var.zone
+  app_disk_image  = var.app_disk_image
 }
 
 module "db" {
-  source           = "../modules/db"
-  public_key_path  = var.public_key_path
-  zone             = var.zone
-  db_disk_image    = var.db_disk_image
-  private_key_path = var.private_key_path
+  source          = "../modules/db"
+  public_key_path = var.public_key_path
+  zone            = var.zone
+  db_disk_image   = var.db_disk_image
 }
 
 module "vpc" {
